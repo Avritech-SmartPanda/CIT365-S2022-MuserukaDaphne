9cd0fae8653266a1ca45c486cf2b9c5d8c616947
Daphne Avril <daphneavril@outlook.com>
2022-04-24 20:40:42 +0200

    Initial commit

---
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/GlazerApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication
+{
+    // Create a new class file called "GlazerApp.cs" and set the class to static.
+    public static class GlazerApp
+    {
+        // Create a new method called "RunExample". Remember that this method must be set to public so that
+        // it can be accessed outside of the GlazerApp class. Make sure to also add the static void keywords.
+        public static void RunExample()
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            // the user is prompted for a width, and assign the width input value to a variable. 
+            Console.Write("Enter width: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            // the user is prompted for a height, and assign the height input value to a variable. 
+            Console.Write("Enter height: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            //  the calculated wood length in feet.
+            woodLength = 2 * (width + height) * 3.25;
+            Console.WriteLine("The length of the wood is " +  woodLength + " feet");
+
+            // glass area in square meters.
+            glassArea = 2 * (width * height);           
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+        }
+    }
+}
---
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MyFirstConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetUserNameAndLocation();
+            ChristmasCountdown(DateTime.Now);
+
+            // Call the RunExample method from the Program Class under Main and place it beneath the ChristmasCountdown call.
+            GlazerApp.RunExample();
+
+            // ensure the application does not automatically terminate and prompt the user to press any key before exiting.
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+        }
+
+        private static void GetUserNameAndLocation()
+        {
+            // Create a new instance of the Person class and give the instance a variable name of "person".
+            Person person = new Person();
+
+            // Prompt the user with a question "What is your name? " and assign their response to the name variable.
+            Console.WriteLine("What is your name?");
+            person.name = Console.ReadLine();
+
+            // Using String Interpolation, reply and prompt the user with "Hi _______! Where are you from? "
+            Console.WriteLine($"Hi {person.name}! Where are you from?");
+
+            // Assign their response to the location attribute of person. 
+            person.location = Console.ReadLine();
+
+            // Using String Interpolation, reply and prompt the user with "I have never been to _______. I bet it is nice. Press any key to continue..."
+            Console.WriteLine($"I have never been to {person.location}. I bet it is nice. Press any key to continue...");
+
+            // Prompt the user to "Press any key to continue." And wait to proceed until a key is pressed.
+            Console.ReadKey();
+        }
+
+        // The method must allow you to pass in a DateTime value when called.
+        private static void ChristmasCountdown(DateTime date)
+        {
+            // Using String Interpolation, output the current date, but not the current time with the following message. "Today's date is: _____"
+            Console.WriteLine($"Today's date is: {DateTime.Now.ToString("MM/dd/yyyy")}");
+
+            // Create your own variable name to store the calculated value of the number of days until Christmas this year as a whole number.
+            var daysTillChristmas = (DateTime.Parse("12/25/2022") - date).Days;
+
+            // Using String Interpolation, output your variable like so "There are ___ days until Christmas!"
+            Console.WriteLine($"There are {daysTillChristmas} days until Christmas!");
+
+            // Prompt the user to "Press any key to continue." And wait to proceed until a key is pressed.
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+        }
+    }
+}
